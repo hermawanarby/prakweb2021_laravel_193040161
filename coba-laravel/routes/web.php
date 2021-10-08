978e4d88632547757ff4c79b7fa69de5ac936f65
hermawanarby <hermawanbaru4@gmail.com>
2021-10-08 15:39:42 +0700

    Menyelesaikan mengenai Struktur Folder, Routes & View pada video ke 3

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Hermawan Arby",
+        "email" => "hermawanarby@gmail.com",
+        "image" => "hermawan.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
