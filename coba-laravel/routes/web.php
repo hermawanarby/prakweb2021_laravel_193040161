45197448c017ca916f3b717c45cb87d682bcc9ae
hermawanarby <hermawanbaru4@gmail.com>
2021-10-14 23:27:24 +0700

    Menyelesaikan video mengenai Model, Collection & Controller

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,55 +30,5 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Hermawan Arby",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad eum repudiandae maxime, perspiciatis mollitia delectus molestias, hic ullam autem temporibus ipsum reprehenderit placeat quidem nisi totam voluptate. Architecto, vel rem."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Riki Sujarno",
-            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nesciunt consectetur eveniet inventore modi sequi cumque neque ullam saepe tempora dolores alias sapiente nobis blanditiis provident dolor possimus placeat voluptatem, distinctio voluptate. Dolorum rem, obcaecati veniam, officia veritatis doloribus nam dicta dolore alias temporibus a sint error fugiat iure? Aut magni minima quae asperiores necessitatibus, ea ipsam ipsum voluptatem neque! Quos at earum error fugiat tempore ducimus quidem, nulla velit, ratione labore dolorum officia ipsa? Libero corrupti, quidem quod nobis quam laborum non ducimus minima beatae dolores. Quidem ea incidunt deleniti quos non! Doloribus recusandae quaerat suscipit? Provident, fugiat autem!"
-        ]
-    ];
-    return view('posts',[
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-
-
-// halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Hermawan Arby",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad eum repudiandae maxime, perspiciatis mollitia delectus molestias, hic ullam autem temporibus ipsum reprehenderit placeat quidem nisi totam voluptate. Architecto, vel rem."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Riki Sujarno",
-            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nesciunt consectetur eveniet inventore modi sequi cumque neque ullam saepe tempora dolores alias sapiente nobis blanditiis provident dolor possimus placeat voluptatem, distinctio voluptate. Dolorum rem, obcaecati veniam, officia veritatis doloribus nam dicta dolore alias temporibus a sint error fugiat iure? Aut magni minima quae asperiores necessitatibus, ea ipsam ipsum voluptatem neque! Quos at earum error fugiat tempore ducimus quidem, nulla velit, ratione labore dolorum officia ipsa? Libero corrupti, quidem quod nobis quam laborum non ducimus minima beatae dolores. Quidem ea incidunt deleniti quos non! Doloribus recusandae quaerat suscipit? Provident, fugiat autem!"
-        ]
-    ];
-
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
