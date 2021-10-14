e1ad5fa0c698cbb4ed0cbfef17a65b827bfa6da6
hermawanarby <hermawanbaru4@gmail.com>
2021-10-15 02:01:56 +0700

    Menyelesaikan video mengenai Post Model

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -2,31 +2,13 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Hermawan Arby",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad eum repudiandae maxime, perspiciatis mollitia delectus molestias, hic ullam autem temporibus ipsum reprehenderit placeat quidem nisi totam voluptate. Architecto, vel rem."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Riki Sujarno",
-            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nesciunt consectetur eveniet inventore modi sequi cumque neque ullam saepe tempora dolores alias sapiente nobis blanditiis provident dolor possimus placeat voluptatem, distinctio voluptate. Dolorum rem, obcaecati veniam, officia veritatis doloribus nam dicta dolore alias temporibus a sint error fugiat iure? Aut magni minima quae asperiores necessitatibus, ea ipsam ipsum voluptatem neque! Quos at earum error fugiat tempore ducimus quidem, nulla velit, ratione labore dolorum officia ipsa? Libero corrupti, quidem quod nobis quam laborum non ducimus minima beatae dolores. Quidem ea incidunt deleniti quos non! Doloribus recusandae quaerat suscipit? Provident, fugiat autem!"
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
