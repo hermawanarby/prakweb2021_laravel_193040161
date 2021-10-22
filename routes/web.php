582db4f554d0f38529c43d70f21b0c7082112509
hermawanarby <hermawanbaru4@gmail.com>
2021-10-23 00:34:56 +0700

    Memperbaiki active navbar dari Home dan About

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Hermawan Arby",
         "email" => "hermawanarby@gmail.com",
         "image" => "hermawan.jpg"
